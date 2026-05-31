70cff4553358193f0db45113f2810a49f456d8c7
adanzee <adan.khurram00@gmail.com>
2026-05-31 18:25:54 +0500

    search node

--- app/agents/search.py
+++ app/agents/search.py
@@ -1,9 +1,20 @@
 from langchain_exa import ExaSearchResults 
+from typing import Any
 import config 
+from app.agents.state import AgentState
+
 
 search_tool = ExaSearchResults(exa_api_key=config.exasearch_api_key, max_results=5)
-results = search_tool.invoke({
-    "query": "Latest AI agent frameworks"
-})
 
-print(type(results))
+def perform_search(state: AgentState):
+    results : dict[str, Any] = {}
+    for sub_query in state["subquery"]:
+        try:
+            output = search_tool.invoke({"query": sub_query})
+            results[sub_query] = output
+        except Exception as e:
+            print(f"Error occurred while performing search for '{sub_query}': {e}")
+    return {
+        "search_result": results
+    }
+        
