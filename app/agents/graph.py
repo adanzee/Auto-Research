39639a1908e49dbf392e852ea6507feaa8841e23
adanzee <adan.khurram00@gmail.com>
2026-06-14 06:42:20 +0500

    updated graph

--- app/agents/graph.py
+++ app/agents/graph.py
@@ -1,24 +1,35 @@
 from langgraph.graph import StateGraph, END, START
 from state import AgentState
+from validation import validate_search_result
+from synthesis import synthesize_results
+from planner import create_research_plan
+from search import perform_search
+
 # correct way of defining nodes and edges in a state graph
 
 workflow = StateGraph(AgentState)
 
 
 def planner_node(state: AgentState):
-    pass
+    return create_research_plan(state)
+    
 
 
 def search_node(state: AgentState):
-    pass
+    return perform_search(state)
+    
+
+
 
 
 def validate_node(state: AgentState):
-    pass
+    return validate_search_result(state)
+    
 
 
 def synthesize_node(state: AgentState):
-    pass
+    return synthesize_results(state)
+    
 
 
 def router_function(state: AgentState):
@@ -27,10 +38,7 @@ def router_function(state: AgentState):
     elif not state["validate_result"] and state["retry_count"] <= 3:
         return "search"
     elif not state["validate_result"] and state["retry_count"] > 3:
-        return "planner"
-    else:
         return END
-
 
 workflow.add_node("planner", planner_node)
 workflow.add_node("search", search_node)
@@ -46,4 +54,4 @@ workflow.add_conditional_edges(
     {"planner": "planner", "synthesize": "synthesize", "search": "search"},
 )
 
-app = workflow.compile()
+graph = workflow.compile()
